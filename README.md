0fb9a725bcb1af10c0eda01aa6c5a8f1265a9f05
siyuqtt <siqiu92@gmail.com>
2014-11-06 19:59:50 -0500

    scaffold built

--- README.md
+++
@@ -1,8 +0,0 @@
-Gmail2Calendar
-==============
-
-CIT597 2014 Fall Group Project <br>
-First Try!
-
-google account : cit597project@gmail.com
-password: cit597penn
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Gmail2Calendar::Application.config.secret_token = 'ec7a5861359df7cf1db76524fb31943dfc58bc5f792ae957dc879fbec8fab5015340103aa8ad2fe157483dd978a01c4facc1fad052bd76a240abb9cfe2aeedcb'
