70fd8ff2e18b0edfc98904ea467ab75c20d96379
siyuqtt <siqiu92@gmail.com>
2014-11-13 13:11:36 -0500

    fix the bug now user can see their own gmails

--- app/controllers/mygmails_controller.rb
+++ app/controllers/mygmails_controller.rb
@@ -1,40 +1,35 @@
 class MygmailsController < ApplicationController
-  attr_accessor :user
-  before_action :set_mygmail_user, only: [:show, :edit, :update, :destroy]
+
+  before_action :set_user #, only: [:show, :edit, :update, :destroy, :]
 
   respond_to :html
   
   def index
-    # if (params[:user_id]) then
-#       @mygmail = Mygmail.where('user_id = ?',params[:user_id])
-#     else
-#       #@mygmails = Mygmail.all
-#     end
-    @user = User.find(params[:user_id])
-    @mygmails = @user.mygmails.all
+   
+    @mygmail = @user.mygmails.all
     respond_with(@user, @mygmail)
   end
 
   def show
-    @user = User.find(params[:user_id])
     @mygmail = @user.mygmails.find(params[:id])
     respond_with(@user, @mygmail)
   end
 
   def new
-    @user = User.find(params[:user_id])
     @mygmail = @user.mygmails.build
     
     respond_with(@user, @mygmail)
   end
 
   def edit
+    @mygmail = @user.mygmails.find(params[:id])
+    respond_with(@user, @mygmail)
   end
 
   def create
-    @user = User.find(params[:user_id])
+   
     @mygmail = @user.mygmails.create(mygmail_params)
-    #Mygmail.new(mygmail_params)
+    
     if @mygmail.save
       respond_with(@user,@mygmail)
     else
@@ -43,24 +38,23 @@ class MygmailsController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:user_id])
+    
     @mygmail = @user.mygmails.find(params[:id])
     @mygmail.update(mygmail_params)
     respond_with(@user, @mygmail)
   end
 
   def destroy
-    # @user = User.find(params[:user_id])
-#     @mygmail = @user.mygmails.find(params[:id])
+    
+    @mygmail = @user.mygmails.find(params[:id])
     @mygmail.destroy
     respond_with(@user, @mygmail)
   end
 
   private
-    def set_mygmail_user
+    def set_user
       @user = User.find(params[:user_id])
-      @mygmail = @user.mygmails.find(params[:id])
-      #@mygmail = Mygmail.find(params[:id])
+     
     end
 
     def mygmail_params
