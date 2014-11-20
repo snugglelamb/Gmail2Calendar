09a35a956a9c928392a51315afb8e3b9b0110c53
siyuqtt <siqiu92@gmail.com>
2014-11-20 16:14:26 -0500

    extract events from gmail no clue how to get the location any idea?

--- app/controllers/mygmails_controller.rb
+++ app/controllers/mygmails_controller.rb
@@ -1,5 +1,6 @@
 require 'gmail'
 require 'date'
+require_relative 'events_controller'
 class MygmailsController < ApplicationController
 
   before_action :set_user #, only: [:show, :edit, :update, :destroy, :]
@@ -13,14 +14,40 @@ class MygmailsController < ApplicationController
       break unless gmail.logged_in?
       gmail.inbox.emails(:after => start ).each do |email|
         if Mygmail.where(:eid => email.uid, :user_id =>@user.id).empty?
+          event_name = email.subject
           text = email.text_part.body.to_s 
+          event_schedule = parsetime text
           @mygmail =  @user.mygmails.create(:eid => email.uid, :user_id => @user.id, :content =>text)
           @mygmail.save
+          @event = @mygmail.events.create(:mygmail_id => @mygmail.id, :schedule => event_schedule, :name=> event_name)
+          @event.save
         end
       end
     end
     @mygmail = @user.mygmails.all
     render :template => "mygmails/index"
+  end
+  
+  def parsetime text
+      text.lines.each do |l| 
+        # 2014-11-20
+        if m = /\d{2,4}-\d{1,2}-\d{1,2}/.match(l)
+          date = DateTime.parse(m[0])
+          return date
+        end
+        #2014/11/20
+        if m = /\d{2,4}\/\d{1,2}\/\d{1,2}/.match(l)
+          date = DateTime.parse(m[0])
+          return date
+        end
+        
+        #11/20
+        if m = /\d{2,4}\/\d{1,2}\/\d{1,2}/.match(l)
+          date = DateTime.parse(m[0])
+          return date
+        end
+        
+    end
   end
   
   def index
