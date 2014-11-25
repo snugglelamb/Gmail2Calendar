9cc8a6c4b87093634d72f4b1c358e22ea1b434a6
siyuqtt <siqiu92@gmail.com>
2014-11-25 02:05:30 -0500

    add length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,8 @@ class User < ActiveRecord::Base
   #attr_accessible :email, :password, :password_confirmation
   has_many :mygmails
   has_many :events , :through => :mygmails
- 
+  validates :psw, length:{ minimum: 4 }
+  
   
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
       data = access_token.info
