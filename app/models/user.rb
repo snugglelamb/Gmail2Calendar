76f66860921ca048c618c1b65af11bc8627fad19
siyuqtt <siqiu92@gmail.com>
2014-11-25 01:56:39 -0500

    delete one validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,7 @@ class User < ActiveRecord::Base
   #attr_accessible :email, :password, :password_confirmation
   has_many :mygmails
   has_many :events , :through => :mygmails
-  validates :psw, length:{ minimum: 4 }
-  
+ 
   
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
       data = access_token.info
