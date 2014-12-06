2c49cc0e5567584a9bb89a89aaca068248c17424
siyuqtt <siqiu92@gmail.com>
2014-12-06 10:00:54 -0500

    TEST ON USER CONTROLLER

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,51 +1,51 @@
-require 'simplecov'
-SimpleCov.start 
-require 'test_helper'
-
-class EventsControllerTest < ActionController::TestCase
-  setup do
-    @event = events(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:events)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create event" do
-    assert_difference('Event.count') do
-      post :create, event: { location: @event.location, mygmail_id: @event.mygmail_id, name: @event.name, shedule: @event.shedule }
-    end
-
-    assert_redirected_to event_path(assigns(:event))
-  end
-
-  test "should show event" do
-    get :show, id: @event
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @event
-    assert_response :success
-  end
-
-  test "should update event" do
-    patch :update, id: @event, event: { location: @event.location, mygmail_id: @event.mygmail_id, name: @event.name, shedule: @event.shedule }
-    assert_redirected_to event_path(assigns(:event))
-  end
-
-  test "should destroy event" do
-    assert_difference('Event.count', -1) do
-      delete :destroy, id: @event
-    end
-
-    assert_redirected_to events_path
-  end
-end
+# require 'simplecov'
+# SimpleCov.start
+# require 'test_helper'
+#
+# class EventsControllerTest < ActionController::TestCase
+#   setup do
+#     @event = events(:one)
+#   end
+#
+#   test "should get index" do
+#     get :index
+#     assert_response :success
+#     assert_not_nil assigns(:events)
+#   end
+#
+#   test "should get new" do
+#     get :new
+#     assert_response :success
+#   end
+#
+#   test "should create event" do
+#     assert_difference('Event.count') do
+#       post :create, event: { location: @event.location, mygmail_id: @event.mygmail_id, name: @event.name, shedule: @event.shedule }
+#     end
+#
+#     assert_redirected_to event_path(assigns(:event))
+#   end
+#
+#   test "should show event" do
+#     get :show, id: @event
+#     assert_response :success
+#   end
+#
+#   test "should get edit" do
+#     get :edit, id: @event
+#     assert_response :success
+#   end
+#
+#   test "should update event" do
+#     patch :update, id: @event, event: { location: @event.location, mygmail_id: @event.mygmail_id, name: @event.name, shedule: @event.shedule }
+#     assert_redirected_to event_path(assigns(:event))
+#   end
+#
+#   test "should destroy event" do
+#     assert_difference('Event.count', -1) do
+#       delete :destroy, id: @event
+#     end
+#
+#     assert_redirected_to events_path
+#   end
+# end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,50 +1,80 @@
+require 'simplecov'
+SimpleCov.start
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  tests User::UsersController
+  include Devise::TestHelpers
+  include Warden::Test::Helpers                        
+    Warden.test_mode!                                    
+  
   setup do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
     @user = users(:one)
+    @request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_auth2] 
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
-  end
+  test "blocks unauthenticated access" do
+    post :create, { user: { email: "user@example.org", password: "password", password_confirmation: "password" } }
+        assert_redirected_to new_user_session_path
+    end
+
+  test "allows authenticated access" do
+      sign_in @user
+      get :index
+     assert_response :success
+    end
+ 
+
+  test "username wrong length" do
+     user = User.find_by_name("swap")
+     user.psw = "12"
+     assert !user.save, "password too short"
+   end
 
   test "should get new" do
+    sign_in @user
     get :new
     assert_response :success
   end
 
-  test "should create user" do
-    assert_difference('User.count') do
-      post :create, user: { account: @user.account, name: @user.name, psw: @user.psw, email: @user.email, created_at: @user.created_at,
-            updated_at: @user.updated_at, encrypted_password: @user.encrypted_password, sign_in_count: @user.sign_in_count}
+  test "non google account user should not log in" do
+    _user = User.new(:email => 'test@example.com', :password => 'password', :password_confirmation => 'password')
+   
+    assert_no_difference('User.count') do
+      post :create, _user
     end
-
-    assert_redirected_to user_path(assigns(:user))
   end
 
   test "should show user" do
+    sign_in @user
     get :show, id: @user
     assert_response :success
+    assert_template "users/show"
   end
 
   test "should get edit" do
+    sign_in @user
     get :edit, id: @user
     assert_response :success
   end
 
   test "should update user" do
+    sign_in @user
     patch :update, id: @user, user: { account: @user.account, name: @user.name, psw: @user.psw }
     assert_redirected_to user_path(assigns(:user))
   end
 
   test "should destroy user" do
+    sign_in @user
     assert_difference('User.count', -1) do
       delete :destroy, id: @user
     end
 
     assert_redirected_to users_path
+  end
+  
+  test "index" do
+    assert_respond_to( @controller, :index )
   end
 end
