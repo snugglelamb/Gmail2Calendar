34e155b9c80039c30ee094e8600f654748b77ba3
siyuqtt <siqiu92@gmail.com>
2014-12-18 14:57:33 -0800

    change validation and view

--- db/schema.rb
+++ db/schema.rb
@@ -14,20 +14,20 @@
 ActiveRecord::Schema.define(version: 20141123121630) do
 
   create_table "events", force: true do |t|
-    t.string   "name"
+    t.string   "name",       limit: nil
     t.datetime "schedule"
-    t.string   "location"
+    t.string   "location",   limit: nil
     t.integer  "mygmail_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_index "events", ["mygmail_id"], name: "index_events_on_mygmail_id"
 
   create_table "mygmails", force: true do |t|
-    t.string   "eid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "eid",        limit: nil
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.integer  "user_id"
     t.text     "content"
   end
@@ -35,24 +35,24 @@ ActiveRecord::Schema.define(version: 20141123121630) do
   add_index "mygmails", ["user_id"], name: "index_mygmails_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "psw"
-    t.string   "account"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "name",                   limit: nil
+    t.string   "psw",                    limit: nil
+    t.string   "account",                limit: nil
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "email",                  limit: nil, default: "", null: false
+    t.string   "encrypted_password",     limit: nil, default: "", null: false
+    t.string   "reset_password_token",   limit: nil
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "token"
+    t.string   "current_sign_in_ip",     limit: nil
+    t.string   "last_sign_in_ip",        limit: nil
+    t.string   "provider",               limit: nil
+    t.string   "uid",                    limit: nil
+    t.string   "token",                  limit: nil
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
